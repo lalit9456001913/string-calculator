b9999ee6a3f4419eaccda288d3fac3146189c2f9
lalit <lk945600@gmail.com>
2025-03-22 22:49:03 +0530

    Throw error for negative numbers, listing all in error message

--- calculator.js
+++ calculator.js
@@ -8,7 +8,15 @@ function add(numbers) {
         numbers = parts[1];
     }
 
-    return numbers.split(delimiter).reduce((sum, num) => sum + parseInt(num, 10), 0);
+    const nums = numbers.split(delimiter).map(num => parseInt(num, 10));
+    const negatives = nums.filter(num => num < 0);
+
+    if (negatives.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+
+    return nums.reduce((sum, num) => sum + num, 0);
 }
+
 
 module.exports = { add }; 
