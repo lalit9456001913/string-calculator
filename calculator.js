589c18a2fb254b8a626c6cd0ee776d5e63edf0ee
lalit <lk945600@gmail.com>
2025-03-22 22:32:05 +0530

    Support custom delimiters with  format

--- calculator.js
+++ calculator.js
@@ -1,6 +1,12 @@
 function add(numbers) {
     if (!numbers) return 0;
-    return numbers
-        .split(/[\n,]/)
-        .reduce((sum, num) => sum + parseInt(num, 10), 0);
+
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(parts[0].substring(2)); // Extract custom delimiter
+        numbers = parts[1];
+    }
+
+    return numbers.split(delimiter).reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
